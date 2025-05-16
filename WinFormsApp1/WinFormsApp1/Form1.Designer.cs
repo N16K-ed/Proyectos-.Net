8785954791d19261e1a010dcdd5f3bf3b9c9aff0
N16K-ed <fsannicceb@educacion.navarra.es>
2025-05-16 13:54:06 +0200

    1 Windows Forms (IE)

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            comboBox1 = new ComboBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            textBox1 = new TextBox();
+            comboBox2 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(253, 152, 0);
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Enabled = false;
+            pictureBox1.ErrorImage = (Image)resources.GetObject("pictureBox1.ErrorImage");
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(127, 81);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(68, 66);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.WaitOnLoad = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Cursor = Cursors.Hand;
+            comboBox1.DropDownHeight = 300;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.IntegralHeight = false;
+            comboBox1.Items.AddRange(new object[] { "Axel Blaze", "Bobby Shearer", "Erik Eagle", "Jack Wallside", "Jim Wraith", "Jude Sharp", "Kevin Dragonfly", "Mark Evans", "Maxwell Carson", "Nathan Swift", "Sam Kincaid", "Steve Grim", "Tim Saunders", "Tod Ironside", "William Glass" });
+            comboBox1.Location = new Point(210, 122);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 24);
+            comboBox1.TabIndex = 6;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(127, 153);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(26, 26);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(167, 163);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(28, 16);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 9F);
+            textBox1.Location = new Point(127, 185);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(204, 230);
+            textBox1.TabIndex = 4;
+            textBox1.TabStop = false;
+            textBox1.Text = "EQUIPO DEL RAIMON";
+            // 
+            // comboBox2
+            // 
+            comboBox2.Cursor = Cursors.Hand;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownWidth = 121;
+            comboBox2.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.IntegralHeight = false;
+            comboBox2.Items.AddRange(new object[] { "Raimon", "Royal Academy", "Zeus" });
+            comboBox2.Location = new Point(210, 81);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 24);
+            comboBox2.TabIndex = 1;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private ComboBox comboBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private TextBox textBox1;
+        private ComboBox comboBox2;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.SelectedIndex = 0;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string seleccion = comboBox1.SelectedItem.ToString();
+
+            switch (seleccion)
+            {
+                case "Axel Blaze":
+                    pictureBox1.Image = Properties.Resources.axel_blaze;
+                    pictureBox2.Image = Properties.Resources.afinidad_FUEGO;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = ["Nuevo y carismático estudiante. Le llaman 'el punta legendario'."];
+                    break;
+                case "Mark Evans":
+                    pictureBox1.Image = Properties.Resources.mark_evans;
+                    pictureBox2.Image = Properties.Resources.afinidad_MONTANA;
+                    pictureBox3.Image = Properties.Resources.PR_icon;
+                    textBox1.Lines = ["Enérgico capitán del Raimon. Nadie vive el fútbol como él."];
+                    break;
+                case "Jude Sharp":
+                    pictureBox1.Image = Properties.Resources.jude_sharp;
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = ["De sus botas sale fútbol de muchos quilates. El mejor en su posición."];
+                    break;
+                case "Kevin Dragonfly":
+                    pictureBox1.Image = Properties.Resources.kevin_dragonfly;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = ["Goleador del Raimon. Algo arisco, pero se deja la piel por el equipo."];
+                    break;
+                case "Nathan Swift":
+                    pictureBox1.Image = Properties.Resources.nathan_swift;
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = ["Defensa veloz como un torbellino. Conoce a Mark desde hace tiempo."];
+                    break;
+                case "Jack Wallside":
+                    pictureBox1.Image = Properties.Resources.jack_wallside;
+                    pictureBox2.Image = Properties.Resources.afinidad_MONTANA;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = ["Su gran cuerpo es como una pared que utiliza para que nadie pase."];
+                    break;
+                case "Tod Ironside":
+                    pictureBox1.Image = Properties.Resources.tod_ironside;
+                    pictureBox2.Image = Properties.Resources.afinidad_FUEGO;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = ["Su cuerpo es pequeño, pero su coraje enorme. Da vidilla al equipo."];
+                    break;
+                case "Tim Saunders":
+                    pictureBox1.Image = Properties.Resources.tim;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = ["Sus gráciles movimientos y dominio del balón son fruto del kung-fu."];
+                    break;
+                case "Jim Wraith":
+                    pictureBox1.Image = Properties.Resources.jim;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = ["Nadie ha podido ver jamás la mirada que esconde detrás de su flequillo."];
+                    break;
+                case "Sam Kincaid":
+                    pictureBox1.Image = Properties.Resources.sam;
+                    pictureBox2.Image = Properties.Resources.afinidad_FUEGO;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = ["Va siempre a su ritmo, aunque los demás le chinchen por ello."];
+                    break;
+                case "Maxwell Carson":
+                    pictureBox1.Image = Properties.Resources.max;
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = ["Por su pinta, nunca dirías que es un as de los deportes."];
+                    break;
+                case "William Glass":
+                    pictureBox1.Image = Properties.Resources.willy_glass;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = ["﻿Un triunfador al que le quedan bien las gafas. O eso dice él..."];
+                    break;
+                case "Steve Grim":
+                    pictureBox1.Image = Properties.Resources.steve;
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = ["No destaca en nada, pero sus habilidades están bien equilibradas."];
+                    break;
+                case "Bobby Shearer":
+                    pictureBox1.Image = Properties.Resources.bobby_shearer;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = ["Es despreocupado, pero pasional. Vivió un tiempo en Estados Unidos."];
+                    break;
+                case "Erik Eagle":
+                    pictureBox1.Image = Properties.Resources.erik_eagle;
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = ["Centrocampista excepcional. Le llaman 'El mago del campo'."];
+                    break;
+                case "Joseph King":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._King;
+                    pictureBox2.Image = Properties.Resources.afinidad_FUEGO;
+                    pictureBox3.Image = Properties.Resources.PR_icon;
+                    textBox1.Lines = new string[] { "Portero - Joseph King" };
+                    break;
+                case "Drent":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Drent; 
+                    pictureBox2.Image = Properties.Resources.afinidad_MONTANA;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = new string[] { "Defensa - Drent" };
+                    break;
+
+                case "Ben Simmons":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Simmons; 
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = new string[] { "Defensa - Ben Simmons" };
+                    break;
+
+                case "Alan Master":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Master; 
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - Alan Master" };
+                    break;
+
+                case "Gus Martin":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Martin; 
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = Properties.Resources.DF_icon;
+                    textBox1.Lines = new string[] { "Defensa - Gus Martin" };
+                    break;
+
+                case "Herman Waldon":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Waldon;  
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - Herman Waldon" };
+                    break;
+
+                case "John Bloom":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Bloom;  
+                    pictureBox2.Image = Properties.Resources.afinidad_FUEGO;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - John Bloom" };
+                    break;
+
+                case "Derek Swing":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Swing;  
+                    pictureBox2.Image = Properties.Resources.afinidad_AIRE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - Derek Swing" };
+                    break;
+
+                case "Daniel Hatch":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Hatch;  
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = new string[] { "Delantero - Daniel Hatch" };
+                    break;
+
+                case "Jude Sharp (2)":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Jude;  
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.MD_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - Jude Sharp" };
+                    break;
+
+                case "David Samford":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources._Samford;  
+                    pictureBox2.Image = Properties.Resources.afinidad_BOSQUE;
+                    pictureBox3.Image = Properties.Resources.DL_icon;
+                    textBox1.Lines = new string[] { "Centrocampista - David Samford" };
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string seleccion = comboBox2.SelectedItem.ToString();
+
+            switch (seleccion)
+            {
+                case "Raimon":
+                    pictureBox1.BackColor = Color.FromArgb(253, 152, 0);
+                    pictureBox1.Image = Properties.Resources.logo_raimon;
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                    textBox1.Lines = ["EQUIPO DEL RAIMON"];
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.AddRange(["Axel Blaze","Bobby Shearer","Erik Eagle","Jack Wallside","Jim Wraith","Jude Sharp",
+                        "Kevin Dragonfly","Mark Evans","Maxwell Carson","Nathan Swift","Sam Kincaid","Steve Grim","Tim Saunders",
+                        "Tod Ironside","William Glass"]);
+                    break;
+                case "Royal Academy":
+                    pictureBox1.BackColor = Color.FromArgb(4, 46, 4);
+                    pictureBox1.Image = Properties.Resources.logo_royal;
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                    textBox1.Lines = ["ROYAL ACADEMY"];
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.AddRange(new string[]
+                    {
+                        "Joseph King",      // Portero
+                        "Drent",            // Defensa
+                        "Ben Simmons",      // Defensa
+                        "Alan Master",      // Centrocampista
+                        "Gus Martin",       // Defensa
+                        "Herman Waldon",    // Centrocampista
+                        "John Bloom",       // Centrocampista
+                        "Derek Swing",      // Centrocampista
+                        "Daniel Hatch",     // Delantero
+                        "Jude Sharp (2)",       // Centrocampista
+                        "David Samford"     // Dl
+                    });
+                    break;
+                case "Zeus":
+                    pictureBox1.BackColor = Color.FromArgb(255, 255, 255);
+                    pictureBox1.Image = Properties.Resources.logo_royal;
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                    textBox1.Lines = ["INSTITUTO ZEUS"];
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.AddRange(new string[]
+                    {
+                        "Poseidon",      // Portero, Masculino, Montaña, Tercer curso, Japón, Apollo
+                        "Paul Siddon",   // Defensa, Masculino, Bosque, Segundo curso, Japón, Hephestus
+                        "Jeff Iron",     // Defensa, Masculino, Fuego, Tercer curso, Japón, Ares
+                        "Lane War",      // Defensa, Masculino, Montaña, Segundo curso, Japón, Dionysus
+                        "Danny Wood",    // Defensa, Masculino, Aire, Primer curso, Japón, Artemis
+                        "Artie Mishman", // Centrocampista, Masculino, Aire, Segundo curso, Japón, Artemis
+                        "Arion Matlock", // Centrocampista, Masculino, Bosque, Segundo curso, Japón, Hermes
+                        "Wesley Knox",   // Centrocampista, Masculino, Bosque, Primer curso, Japón, Athena
+                        "Jonas Demetrius", // Delantero, Masculino, Fuego, Segundo curso, Japón, Demeter
+                        "Byron Love",    // Centrocampista, Masculino, Bosque, Segundo curso, Japón, Aphrodite
+                        "Henry House"    // Centrocampista, Masculino, Bosque, Segundo curso, Japón, Hera
+                    });
+                    break;
+            }            
+        }
+    }
+}

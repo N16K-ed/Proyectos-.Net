b4fbc83571d24f67b7fa653c6869e517cf8b8c58
N16K-ed <fsannicceb@educacion.navarra.es>
2025-05-21 10:12:32 +0200

    chat consola

---
+++ Chat TCP/Chat TCP/Server.cs
@@ -0,0 +1,105 @@
+﻿// -----------------------------------------------
+// Servidor de Chat TCP multicliente en C#
+// Con identificación del usuario de Windows
+// -----------------------------------------------
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpChatServer
+{
+    static Dictionary<TcpClient, string> clients = new Dictionary<TcpClient, string>();
+
+    static object lockObject = new object();
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("Servidor de chat iniciado en puerto 5000...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        try
+        {
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string userName = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+
+            lock (lockObject)
+            {
+                clients.Add(client, userName);
+            }
+
+            Console.WriteLine($"[{userName}] se ha conectado.");
+
+            BroadcastMessage($"*** {userName} se ha unido al chat ***", client);
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string mensaje = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"[{userName}]: {mensaje}");
+
+                BroadcastMessage($"[{userName}]: {mensaje}", client);
+            }
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            lock (lockObject)
+            {
+                if (clients.ContainsKey(client))
+                {
+                    string userName = clients[client];
+                    clients.Remove(client);
+
+                    Console.WriteLine($"[{userName}] se ha desconectado.");
+
+                    BroadcastMessage($"*** {userName} ha salido del chat ***", client);
+                }
+            }
+            client.Close();
+        }
+    }
+
+    static void BroadcastMessage(string message, TcpClient sender)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+
+        lock (lockObject)
+        {
+            foreach (var client in clients.Keys)
+            {
+                if (client != sender)
+                {
+                    try
+                    {
+                        NetworkStream stream = client.GetStream();
+                        stream.Write(data, 0, data.Length);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Cliente/Cliente/Client.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------
+// Cliente de Chat TCP multicliente en C#
+// Se conecta al servidor y envía nombre de usuario
+// -----------------------------------------------
+
+using System.Net.Sockets;
+using System.Text;
+
+class TcpChatClient
+{
+    static void Main()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+
+            string userName = Environment.UserName;
+            byte[] userNameData = Encoding.UTF8.GetBytes(userName);
+            stream.Write(userNameData, 0, userNameData.Length);
+
+            Console.WriteLine($"Conectado como {userName}. Escribe mensajes para enviar.");
+            Console.WriteLine("Escribe 'salir' para desconectarte.");
+
+            Thread receiveThread = new Thread(() =>
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                try
+                {
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        string mensaje = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Console.WriteLine("\n" + mensaje);
+                        Console.Write("> ");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nDesconectado del servidor.");
+                }
+            });
+
+            receiveThread.Start();
+
+            while (true)
+            {
+                Console.Write("> ");
+                string mensaje = Console.ReadLine();
+
+                if (mensaje.ToLower() == "salir")
+                    break;
+
+                byte[] data = Encoding.UTF8.GetBytes(mensaje);
+                stream.Write(data, 0, data.Length);
+            }
+
+            client.Close();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Error: " + exception.Message);
+        }
+    }
+}
